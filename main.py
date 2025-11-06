19b9c046a525a1d6b8c146e9cc166c80ab58583d
Chonghao Ju <chonghao.ju@gmail.com>
2025-11-06 22:28:52 +0800

    Update index.html and main.py

--- main.py
+++ main.py
@@ -2290,16 +2290,6 @@ def render_html_content(
         html += """
                 </div>"""
 
-    html += """
-            </div>
-            
-            <div class="footer">
-                <div class="footer-content">
-                    由 <span class="project-name">TrendRadar</span> 生成 · 
-                    <a href="https://github.com/sansan0/TrendRadar" target="_blank" class="footer-link">
-                        GitHub 开源项目
-                    </a>"""
-
     if update_info:
         html += f"""
                     <br>
